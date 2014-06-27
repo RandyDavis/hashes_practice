0cc2e3940f80ab9a77aaeeb61c9ace1907842bc4
Randy Davis <randy_davis@outlook.com>
2014-06-27 12:17:04 -0500

    Update hash_step15.rb with a question comment and complete hash_step16.rb file has solution.

--- hash_step15.rb
+++ hash_step15.rb
@@ -1,3 +1,5 @@
 hash = {"name" => "bob", :job => "builder", 21 => "current_age"}
 # TODO: Print each of the values in the hash. Use the keys (you don't need to use a loop)
 p hash.values
+
+#Use the keys??
---
+++ hash_step16.rb
@@ -0,0 +1,26 @@
+# TODO: Print each of the values in the hash. Use the keys (don't use loops)
+hash = {
+	"name" => {
+		:first => "Louis",
+		:last => "The Thirteenth"
+	},
+	"father" => {
+		"name" => {
+			:first => "Louis",
+			:last => "The Twelvth"
+		},
+		"father" => {
+			"name" =>{
+				:first => "Louis",
+				:last => "The Eleventh"
+			},
+			"father" => "You've gotta be kidding me!"
+		}
+	}
+}
+
+# p hash["name"][:first],["name"][:last],["father"]["name"][:first],["father"]["name"][:last]
+p hash["name"].values
+p hash["father"]["name"].values
+p hash["father"]["father"]["name"].values
+p hash["father"]["father"]["father"]
